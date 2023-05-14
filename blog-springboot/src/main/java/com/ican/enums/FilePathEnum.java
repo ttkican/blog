a35787dfff55e6d44e541e92a31b18a4ccaf34ec
1632167813@qq.com <1632167813@qq.com>
2023-05-14 12:30:00 +0800

    修改图片上传路径

--- blog-springboot/src/main/java/com/ican/enums/FilePathEnum.java
+++ blog-springboot/src/main/java/com/ican/enums/FilePathEnum.java
@@ -15,27 +15,27 @@ public enum FilePathEnum {
     /**
      * 头像路径
      */
-    AVATAR("/avatar/", "/avatar", "头像路径"),
+    AVATAR("avatar/", "/avatar", "头像路径"),
 
     /**
      * 文章图片路径
      */
-    ARTICLE("/article/", "/article", "文章图片路径"),
+    ARTICLE("article/", "/article", "文章图片路径"),
 
     /**
      * 配置图片路径
      */
-    CONFIG("/config/", "/config", "配置图片路径"),
+    CONFIG("config/", "/config", "配置图片路径"),
 
     /**
      * 说说图片路径
      */
-    TALK("/talk/", "/talk", "说说图片路径"),
+    TALK("talk/", "/talk", "说说图片路径"),
 
     /**
      * 照片路径
      */
-    PHOTO("/photo/", "/photo", "相册路径");
+    PHOTO("photo/", "/photo", "相册路径");
 
     /**
      * 路径
