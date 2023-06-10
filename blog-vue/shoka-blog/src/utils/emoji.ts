afa023a8c0338cdd9cb18c3b62f2922c201c2dbb
1632167813@qq.com <1632167813@qq.com>
2023-06-10 17:56:51 +0800

    完善表情功能

--- blog-vue/shoka-blog/src/utils/emoji.ts
+++ blog-vue/shoka-blog/src/utils/emoji.ts
@@ -73,6 +73,4 @@ export default {
   "[原神_哼]": "https://static.ttkwsd.top/emoji/heng.png",
   "[原神_喝茶]": "https://static.ttkwsd.top/emoji/hecha.png",
   "[原神_生气]": "https://static.ttkwsd.top/emoji/kqsq.png",
-  "[tv_微笑]": "https://static.ttkwsd.top/emoji/tvwx.png",
-  "[tv_斜眼笑]": "https://static.ttkwsd.top/emoji/tvxyx.png",
 };
