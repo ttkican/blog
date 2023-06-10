0e2b63465149d433bd5be6ab5e0d87aa23123399
1632167813@qq.com <1632167813@qq.com>
2023-06-10 17:59:23 +0800

    优化代码

--- blog-vue/shoka-blog/src/assets/js/config.ts
+++ blog-vue/shoka-blog/src/assets/js/config.ts
@@ -2,9 +2,13 @@ export default {
   // gitee的client-id
   GITEE_APP_ID: "***************************",
   // gitee回调地址
-  GITEE_REDIRECT_URI: "***************************",
+  GITEE_REDIRECT_URL: "***************************",
   // github的client-id
   GITHUB_APP_ID: "***************************",
   // github回调地址
   GITHUB_REDIRECT_URL: "***************************",
+  // qq的app-id
+  QQ_APP_ID: "***************************",
+  // qq的回调地址
+  QQ_REDIRECT_URL: "***************************",
 };
