c8ed2ebddea17c1df0cf87cb69d10fd44c9a6b87
YuNanXing <1224678297@qq.com>
2023-08-23 11:46:57 +0800

    feat: 优化打开文章时路由跳转

--- blog-vue/shoka-blog/src/router/index.ts
+++ blog-vue/shoka-blog/src/router/index.ts
@@ -133,6 +133,18 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  //滚动行为
+  scrollBehavior(to, from, savedPosition) {
+    // to：即将进入的路由对象
+    // from：当前导航正要离开的路由对象
+    // savedPosition：上次记录的滚动位置
+    // 默认行为，如果有记录的滚动位置，则恢复到该位置
+    if (savedPosition) {
+      return savedPosition;
+    }
+    // 没有记录的滚动位置，则滚动到页面顶部
+    return { top: 0 };
+  },
 });
 
 export default router;
