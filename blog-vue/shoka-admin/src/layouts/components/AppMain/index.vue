471e05b1282aee02e5b355700de2343cc56b2d77
1632167813@qq.com <1632167813@qq.com>
2024-06-22 01:52:30 +0800

    去除keep-alive缓存

--- blog-vue/shoka-admin/src/layouts/components/AppMain/index.vue
+++ blog-vue/shoka-admin/src/layouts/components/AppMain/index.vue
@@ -2,9 +2,7 @@
     <section class="app-main">
         <router-view v-slot="{ Component, route }">
             <transition name="fade-transform" mode="out-in">
-                <keep-alive>
-                    <component :is="Component" :key="route.fullPath" />
-                </keep-alive>
+                <component :is="Component" :key="route.fullPath" />
             </transition>
         </router-view>
     </section>
